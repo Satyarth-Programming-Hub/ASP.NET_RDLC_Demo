e1d364208a013cb9ebf0b995471758a76ce8eb82
TechTalkSaty <noreply@example.com>
2021-07-23 13:57:13 +0530

    Add project files.

---
+++ ASP.NET_RDLC_Demo/UI/Index.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ASP.NET_RDLC_Demo.UI.Index" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <div>
+            <asp:TextBox ID="txtCountry" runat="server" /><br />
+            <asp:Button ID="btnLoadReport" Text="Load Report" runat="server" OnClick="btnLoadReport_Click" />
+        </div>
+        <div>
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server" Height="489px" Width="1380px"></rsweb:ReportViewer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET_RDLC_Demo/UI/Index.aspx.cs
@@ -0,0 +1,48 @@
+﻿using ASP.NET_RDLC_Demo.ReportDataSets;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ASP.NET_RDLC_Demo.UI
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnLoadReport_Click(object sender, EventArgs e)
+        {
+            ReportViewer1.ProcessingMode = ProcessingMode.Local;
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Reports/EmployeeDetails.rdlc");
+            Employees employees = GetData();
+            ReportDataSource dataSource = new ReportDataSource("Employee", employees.Tables[0]);
+            ReportViewer1.LocalReport.DataSources.Clear();
+            ReportViewer1.LocalReport.DataSources.Add(dataSource);
+        }
+
+        private Employees GetData()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            string query = "SELECT * FROM Employee WHERE country=@Country";
+
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@Country", txtCountry.Text);
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    da.SelectCommand = cmd;
+                    using (Employees employees = new Employees())
+                    {
+                        da.Fill(employees, "Emp");
+                        return employees;
+                    }
+                }
+            }
+        }
+    }
+}
